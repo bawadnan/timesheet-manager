b5c7afdd9fe2789a2106d71fd8965d15f24bef49
nish.naidoo <nish.naidoo@localhost>
2013-03-01 00:44:19 +1100

    Generated a working test case.

---
+++ src/org/nishen/timesheet/bean/Result.java
@@ -0,0 +1,8 @@
+package org.nishen.timesheet.bean;
+
+public class Result
+{
+	public static final String SUCCESS = "success";
+	public static final String FAILURE = "failure";
+	public static final String ERROR = "error";
+}
---
+++ src/org/nishen/timesheet/bean/TestBean.java
@@ -0,0 +1,48 @@
+package org.nishen.timesheet.bean;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestBean
+{
+	private static final Logger log = LoggerFactory.getLogger(TestBean.class);
+
+	private String name;
+	private int age;
+
+	public TestBean()
+	{
+		log.debug("instantiating class: {}", this.getClass().getName());
+
+		name = "Nishen";
+		age = 37;
+	}
+	
+	public String update()
+	{
+		log.debug("age: {}", age);
+		age += 1;
+		
+		return Result.SUCCESS;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public int getAge()
+	{
+		return age;
+	}
+
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+}
