81d2c902c6f3501e126497468f2b6f48ad830061
nish.naidoo <nish.naidoo@localhost>
2013-09-29 12:28:08 +1000

    Starting 'rest' branch

--- src/org/nishen/timesheet/bean/Result.java
+++
@@ -1,8 +0,0 @@
-package org.nishen.timesheet.bean;
-
-public class Result
-{
-	public static final String SUCCESS = "success";
-	public static final String FAILURE = "failure";
-	public static final String ERROR = "error";
-}
